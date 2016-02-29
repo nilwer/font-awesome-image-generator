0d61fa6b2e47f5d168e63bdd01fd05065823bd91
Nils Werner <Nils Werner>
2016-02-29 12:55:01 +0100

    add font, rename file

--- index.php
+++ index.php
@@ -12,16 +12,16 @@ $background = imagecolorallocatealpha($img, 0, 0, 0, 127);
 //set backgroundcolor
 imagefill($img, 0, 0, $background);
 
-$hex = "#ff9900";
+if(isset($_GET['color']))$hexcolor = $_GET['color'];
 //convert hex to rgb color
-list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x");
+list($r, $g, $b) = sscanf($hexcolor, "%02x%02x%02x");
 //generate rgb fontcolor
 $fontcolor = imagecolorallocatealpha($img, $r, $g, $b, 0);
 
 //set fonticon as unicode
 $text = json_decode('"&#xF041;"');
 //set path to font
-$font = '../fonts/fontawesome/fontawesome-webfont.ttf';
+$font = './fontawesome-webfont.ttf';
 //insert icon into image
 imagettftext($img, 42, 0, 1, 45, $fontcolor, $font, $text);
 
